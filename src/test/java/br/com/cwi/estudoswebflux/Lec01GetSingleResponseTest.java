9901de138dc4852b77acd5d88fc3ddc6d2d9e02d
Nicolai Poletto <nicolaipoletto@yahoo.com.br>
2021-05-06 18:00:59 -0300

    Iniciando parte de Webclient

---
+++ src/test/java/br/com/cwi/estudoswebflux/Lec01GetSingleResponseTest.java
@@ -0,0 +1,11 @@
+package br.com.cwi.estudoswebflux;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class Lec01GetSingleResponseTest extends BaseTest {
+
+    @Autowired
+    private WebClient webClient;
+
+}
